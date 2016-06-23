da3066e0a47f71ce593870b2e5c88f52bf758162
Guillermo Calvo <guillermocalvo@yahoo.com>
2016-06-23 17:19:57 +0200

    Add processing-js-sketch by downloading from HyperDev

---
+++ processing-js-sketch/public/client.js
@@ -0,0 +1,12 @@
+// client-side js
+// run by the browser each time your view template is loaded
+
+// protip: you can rename this to use .coffee if you prefer
+
+// by default, you've got jQuery,
+// add other scripts at the bottom of index.html
+
+$(function() {
+  console.log('hello world :o');
+
+});
